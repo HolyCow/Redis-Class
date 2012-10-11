07faeb7e3522bd1a51166598b8ade5207e2d54d6
Mike Beasterfeld <mike.beasterfeld@gmail.com>
2012-10-11 04:55:12 -0500

    set up backend framework

---
+++ lib/Redis/Class/Backend.pm
@@ -0,0 +1,85 @@
+package Redis::Class::Backend;
+
+use 5.006;
+use strict;
+use warnings;
+
+use Moose;
+use namespace::autoclean;
+
+=head1 NAME
+
+Redis::Class::Backend - Determines backend to use for Redis::Class
+
+=head1 VERSION
+
+Version 0.0001
+
+=cut
+
+our $VERSION = '0.0001';
+
+
+=head1 SYNOPSIS
+
+
+=head1 AUTHOR
+
+Mike Beasterfeld, C<< <mike.beasterfeld at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-redis-class at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Redis-Class>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Redis::Class
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Redis-Class>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Redis-Class>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Redis-Class>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Redis-Class/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Mike Beasterfeld.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Redis::Class
